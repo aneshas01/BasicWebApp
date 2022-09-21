494ee2e21a3262503b7cd366d6c307d7cc3135dd
Anesha Santhanam <aneshas@LAPTOP-CK7JVJCB.localdomain>
2022-09-20 22:47:37 -0400

    modified team response to satisfy test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,8 +8,8 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (query.toLowerCase().contains("name")) {
-            return "MyTeam";
+        if (query.toLowerCase().contains("team")) {
+            return "Team Name";
         }
         return "";
     }
